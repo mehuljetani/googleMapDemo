1af73ad5e11366064406089b2ebaaea87eeca752
mehuljetani <mehuljetani23@gmail.com>
2022-07-22 18:14:20 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+        mapType={'satellite'}
+        style={styles.map}
+        region={{
+          latitude: 32.2432,
+          longitude: 77.1892,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}></MapView>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});

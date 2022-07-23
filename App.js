a278b66872190614a867c68c284cfbc7aa5659a1
mehuljetani <mehuljetani23@gmail.com>
2022-07-23 10:59:28 +0530

    commit after all done

--- App.js
+++ App.js
@@ -24,8 +24,8 @@ export default App;
 const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
-    height: 400,
-    width: 400,
+    // height: 400,
+    // width: 400,
     justifyContent: 'flex-end',
     alignItems: 'center',
   },
